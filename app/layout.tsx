63ba6df49080342c0204fe9d6d50cad436c4b011
moinahmed97 <59069225+moinahmed97@users.noreply.github.com>
2024-11-05 14:51:13 -0500

    added additional code for speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Script from 'next/script';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 import "./globals.css";
@@ -29,6 +30,7 @@ export default function RootLayout({
       </head>
       <body>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
